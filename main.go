6840c3ac85bbec5d3a682993f72bdbc1d9292c4b
pro911 <pro911@qq.com>
2022-09-09 11:44:34 +0800

    1

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"echo/app/controllers/echo"
+	pb "echo/grpc/proto"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"net"
 )
+
+type UserInfoService struct {
+	pb.UnimplementedUserInfoServiceServer
+}
+
+var u = UserInfoService{}
 
 func main() {
 	serve := gin.Default()
@@ -19,4 +28,21 @@ func main() {
 	if err := serve.Run(":8000"); err != nil {
 		fmt.Printf("startup service failed, err:%v\n\n", err)
 	}
+
+	// 1. 监听
+	address := "0.0.0.0:30005"
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("监听异常 ", err)
+		return
+	} else {
+		fmt.Println("监听成功:", address)
+	}
+	// 2. 实例化rpc
+	s := grpc.NewServer()
+	// 3. 在gRPC上注册微服务
+	// 第一个类型是服务，第二个类型是接口的变量
+	pb.RegisterUserInfoServiceServer(s, &u)
+	// 4. 启动gRPC服务端
+	_ = s.Serve(lis)
 }
